c0e600d057c7dd5af4eff44a7b759c7d315e01aa
Han Eunjin <haneunjin255@gmail.com>
2022-11-29 13:26:30 +0900

    item market 작업

--- client/src/pages/ItemMarket.js
+++ client/src/pages/ItemMarket.js
@@ -1,22 +1,60 @@
-import React from 'react'
+import React, {useContext, useEffect} from 'react'
 import Header from '../components/Header'
 import '../style/ItemMarket.css'
+import NFTItemlist from '../components/ERC721-item/NFTItemList'
+import Box from '../icon/Box.png'
+import axios from 'axios'
+import {UserContext} from '../User/UserContext'
 
 const ItemMarket = () => {
+    const {user, setUser, isLogin, setIsLogin} = useContext(UserContext);
+    useEffect(() => {
+        axios.get("http://localhost:8000/user/success",
+            {withCredentials : true})
+            .then(function (response) {
+                console.log("MyPage success")
+                // console.log(response.data)
+                setIsLogin(true)    // 로그인 상태 유지
+                // 유저정보를 갱신함
+                setUser({
+                    id: response.data.id,
+                    user_address: response.data.user_address,
+                    user_nickname: response.data.user_nickname,
+                    user_token1amount: response.data.user_token1amount,
+                    user_token2amount: response.data.user_token2amount,
+                    user_score: response.data.user_score,
+                    user_img: response.data.user_img,
+                })
+            })
+            .catch((Error) => {
+                console.log(Error)
+            })
+        },[])
  return(
   <div className='ItemMarket'>
     <Header/>
         <div className='item_Content'>
             <div className='randomImg'>
-                <div className='randomImgBox'>randombox</div>
+                <div className='randomImgBox'>
+                    <img src={Box} alt='Box'></img>
+                </div>
             </div>
             <div className='itemInfo'>
-                <div className='transactionButton'><button>Transaction button</button></div>
-                <div className='token1Count'>ERC20 Token1 Count</div>
-                <div className='marketDescription'>Market Description</div>
+                <div className='transactionButton'><button>Transaction Button</button></div>
+                <div className='token1Count'>
+                    <div>ERC-20 Token1 Count : {user.user_token1amount}개</div>
+                </div>
+                <div className='marketDescription'>
+                    <div className='description'>
+                        <span>Token을 사용하여 랜덤으로 아이템을 뽑으세요!</span><br/><br/>
+                        <span>위에 있는 노란 버튼을 통해 랜덤 뽑기가 가능합니다.</span><br/><br/>
+                        <span>뽑은 아이템을 프로필에 장착하여 기본 점수를 높일 수 있습니다.</span><br/><br/>
+                        <span>랜덤으로 뽑힌 아이템과 상세 설명은 마이 페이지에서 확인할 수 있습니다.</span><br/>
+                    </div>
+                </div>
             </div>
             <div className='imgCollect'>
-                <div>imgCards</div>
+                <div className='NFTs'><NFTItemlist/></div>
             </div>
         </div>
   </div>
